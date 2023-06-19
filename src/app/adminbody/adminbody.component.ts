d4d532e66266b89377175ad7296872cefb0ae61d
Aleena <elzabathaleena@gmail.com>
2023-06-19 09:47:40 +0530

    first commits

---
+++ src/app/adminbody/adminbody.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminbody',
+  templateUrl: './adminbody.component.html',
+  styleUrls: ['./adminbody.component.css']
+})
+export class AdminbodyComponent {
+
+}
